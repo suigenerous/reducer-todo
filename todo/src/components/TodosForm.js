483ca8b1cb710863c789b29586502fe58bc6cbf9
William Herman <williamajherman@gmail.com>
2020-09-07 15:33:50 -0400

    add todo to list

--- todo/src/components/TodosForm.js
+++ todo/src/components/TodosForm.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function TodosForm(props) {
-
+    const [text, setText] = useState("");
     return (
         <div>
+            <input
+                value = {text}
+                onChange = {e => setText(e.target.value)}
+            />
+            <button onClick={() => {props.add(text); setText("")}}>Add Todo</button>
+
         </div>
     );
 }
--- todo/src/components/TodosList.js
+++ todo/src/components/TodosList.js
@@ -1,21 +1,29 @@
 import React, { useReducer } from 'react';
 import { todoReducer, initialState } from '../reducers/reducers';
 import Todo from './Todo';
+import TodosForm from './TodosForm';
 
 export default function TodosList(props){
     const [state, dispatch] = useReducer(todoReducer, initialState);
 
     return(
         <div>
-            {state.todosArr.map(t => (
-                <Todo
-                    key = {t.id}
-                    todo = {t}
-                    remove = {() => dispatch({type: "REMOVE", id: t.id })}
-                    toggleComplete = {t.completed === false ? () => dispatch({type: "MARK_COMPLETE", id: t.id}) : () => dispatch({type: "MARK_INCOMPLETE", id: t.id})
-                    }
+            <div>
+                <TodosForm
+                    add={text => dispatch({type: "ADD", text: text})}
                 />
-            ))}
+            </div>
+            <div>
+                {state.todosArr.map(t => (
+                    <Todo
+                        key = {t.id}
+                        todo = {t}
+                        remove = {() => dispatch({type: "REMOVE", id: t.id })}
+                        toggleComplete = {t.completed === false ? () => dispatch({type: "MARK_COMPLETE", id: t.id}) : () => dispatch({type: "MARK_INCOMPLETE", id: t.id})
+                        }
+                    />
+                ))}
+            </div>
         </div>
     )
 }
